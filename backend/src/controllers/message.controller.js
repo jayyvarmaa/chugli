06c27a4a3c9bd094e4448aba2bd96846e58e0cdf
Jay Varma <jaymayurvarma@gmail.com>
2026-06-06 00:38:17 +0530

    fix: correct Message.js import casing for Linux compatibility

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import Message from "../models/message.model.js";
+import Message from "../models/Message.js";
 import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
 import mongoose from "mongoose";
